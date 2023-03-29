d15460da2aab4f36997a3f7ed634f81ffa6b0b02
ozandilah <79375569+ozandilah@users.noreply.github.com>
2023-03-29 15:13:16 +0700

    memperbaiki navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Fauzan &nbsp;
-            <span className="sm:block hidden"> | Javascript Mastery</span>
+            <span className="sm:block hidden"> | Member Ikode</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
